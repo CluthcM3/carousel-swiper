cd7f2d3ac9485155d2b2986d952e6151683c6ab5
CluthcM3 <spammerid@hotmail.com>
2023-01-04 23:20:28 +0700

    remove components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Main from "./components/Main";
-import MinandMax from "./components/MinandMax";
 import SwiperCarousel from "./components/SwiperCarousel";
 
 function App() {
   return (
     <div className="m-8 ">
-      {/* <Header />
-      <Main />
-      <Footer /> */}
-      {/* <MinandMax /> */}
       <SwiperCarousel />
     </div>
   );
